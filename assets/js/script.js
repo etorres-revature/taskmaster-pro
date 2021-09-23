15adf342badb63d4de6df626e860a65b36934203
Eric D. Torres <etorresnotary@gmail.com>
2021-09-22 21:57:47 -0500

    auditing tasks for due date and styling li appropriately

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 let tasks = {};
 
-let createTask = function (taskText, taskDate, taskList) {
+const auditTask = function (taskEl) {
+  // get date from task element
+  let date = $(taskEl).find("span").text().trim();
+
+  //convert to moment object at 5:00pm
+  let time = moment(date, "L").set("hour", 17);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+const createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   let taskLi = $("<li>").addClass("list-group-item");
   let taskSpan = $("<span>")
@@ -11,11 +29,14 @@ let createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
 
-let loadTasks = function () {
+const loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -37,7 +58,7 @@ let loadTasks = function () {
   });
 };
 
-let saveTasks = function () {
+const saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
@@ -119,6 +140,9 @@ $(".list-group").on("change", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into audtiTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
